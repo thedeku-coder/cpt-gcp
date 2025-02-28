05adfb830b48bf481c1e281a09f23f9e9fec2935
Valentin hebert <valentin.hebert2.etu@univ-lille.fr>
2025-02-28 12:09:10 +0100

    code modifier

--- app/app.py
+++ app/app.py
@@ -1,13 +1,11 @@
 from flask import Flask, render_template, request
 
-app = Flask(__name__ , template_folder='templates')
+app = Flask(__name__ )
 
-@app.route('/')
-def index():
+@app.route('/app<var>')
+def index(var):
 
-    return render_template('index.html', message="helloword")
-
-
+    return f'Hello {var}!'
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
