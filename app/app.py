962faf0ed7d5338f291dd29a2c9f0855a7d657a1
Valentin hebert <valentin.hebert2.etu@univ-lille.fr>
2025-02-28 12:32:09 +0100

    fafa

--- app/app.py
+++ app/app.py
@@ -2,10 +2,12 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/app<var>')
-def index(var):
-
-    return f'Hello {var}!'
+@app.route('/app')
+def index():
+    params = request.args
+    name = params.get('name')
+    return f'Hello {name}!'
+    
 
 if __name__ == '__main__':
     app.run(debug=True, port=8080, host='0.0.0.0')
